182775f8366a6a68ebc1fb003c7292846b9f3c61
githubtome <welcometotheworld.lkm@gmail.com>
2018-08-26 16:27:55 +0800

    数据源由h2改为MySQL

--- src/main/java/com/example/OnboardingRequest.java
+++ src/main/java/com/example/OnboardingRequest.java
@@ -73,10 +73,10 @@ public class OnboardingRequest {
 
     private static void testActivi() throws ParseException {
         ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
-                .setJdbcUrl("jdbc:h2:mem:activiti;DB_CLOSE_DELAY=1000")
-                .setJdbcUsername("sa")
-                .setJdbcPassword("")
-                .setJdbcDriver("org.h2.Driver")
+                .setJdbcUrl("jdbc:mysql://127.0.0.1:3306/demo?useUnicode=true&amp;characterEncoding=utf-8")
+                .setJdbcUsername("root")
+                .setJdbcPassword("root")
+                .setJdbcDriver("com.mysql.jdbc.Driver")
                 .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
         ProcessEngine processEngine = cfg.buildProcessEngine();
         String pName = processEngine.getName();
